4575e022c420b9cb91a3c78753eb064da7c0a02b
byeol-bit <sshkww@naver.com>
2026-03-13 10:33:38 +0900

    React 프로젝트 추가

--- app/src/pages/ReactProjectsPage.tsx
+++ app/src/pages/ReactProjectsPage.tsx
@@ -8,7 +8,7 @@ const ReactProjectsPage = () => {
       title="React 프로젝트"
       subtitle="React로 개발한 프론트엔드 프로젝트들"
     >
-      <ProjectList projects={reactProjects} enableGallery={false} />
+      <ProjectList projects={reactProjects} />
     </ProjectPageLayout>
   )
 }
